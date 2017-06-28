1416ab50d871c1ebb3d3a2d458db1de3179ce0be
mkhullar <mkhullar@asu.edu>
2017-06-28 14:58:02 -0700

    fixed redirection

--- Yelp/urls.py
+++ Yelp/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 from Charts import urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^charts/', include('Charts.urls')),
+    url(r'^charts/', include('Charts.urls', namespace='charts')),
+    url(r'^$', RedirectView.as_view(url='/charts/', permanent=True)),
 ]
